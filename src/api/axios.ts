36ff814f683f84a04aec160b9eac7831966cd3d6
nadobrranichh <nazarshch26@gmail.com>
2025-05-22 15:31:46 +0300

    Connected Axios library

---
+++ src/api/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://vetra-8c5dfe3bdee7.herokuapp.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
