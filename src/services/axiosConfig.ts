276a875cc7ffed1ebf08d98b00fc7628267cf48c
idenyy <d3nys.pavlov@gmail.com>
2025-06-12 00:10:59 +0300

    refactor: remove redundant headers config in axios instance setup

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://vetra-8c5dfe3bdee7.herokuapp.com',
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 axiosInstance.interceptors.request.use(
@@ -35,4 +32,4 @@ axiosInstance.interceptors.response.use(
   }
 );
 
-export default axiosInstance; 
+export default axiosInstance;
